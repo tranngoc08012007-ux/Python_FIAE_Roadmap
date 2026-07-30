a346ddc4abd8dbc3e061e60ad96464ff32545eb9
Tran Ngoc <tranngoc08012007@gmail.com>
2026-07-30 18:41:28 +0700

    feat: add Lesson 01 exercises

---
+++ Phase_01 — Python Fundamentals/Lesson_01_Environment_Setup/exercises.py
@@ -0,0 +1,98 @@
+# ==========================================================
+# Lesson 01 Exercises
+# Topic: Environment Setup & Hello World
+# ==========================================================
+
+# Instructions:
+# - Complete all exercises by yourself.
+# - Do NOT copy code from main.py.
+# - Use comments when necessary.
+
+# ----------------------------------------------------------
+# Exercise 1 - Hello, World!
+# Print the following text:
+# Hello, World!
+
+# ----------------------------------------------------------
+# Exercise 2 - Simple Math
+# Print the results of:
+#  15 + 25
+#  100 - 36
+#  8 * 7
+#  81 / 9
+
+# ----------------------------------------------------------
+# Exercise 3 - Personal Information
+# Create variables: name, birth_year, current_year, city, target_job.
+# Print all variables.
+
+# ----------------------------------------------------------
+# Exercise 4 - Calculate Your Age
+# Using the variables above, calculate and print your age.
+
+# ----------------------------------------------------------
+# Exercise 5 - Years Until Ausbildung
+# Create: ausbildung_year = 2027.
+# Calculate and print how many years are left.
+
+# ----------------------------------------------------------
+# Exercise 6 - Update a Variable
+# Increase your age by 1 without writing age = 20.
+# Update the current variable, then print the new value.
+
+# ----------------------------------------------------------
+# Exercise 7 - Snake Case Practice
+# Create variables: favorite_food, favorite_programming_language, phone_number.
+# Print all variables.
+# ==========================
+# My solutions
+# ==========================
+# Excercise 1:
+print("Hello, world!") 
+# ==========================
+# Excercise 2:
+print(15 +25)
+print(100 -36)
+print(8 * 7)
+print(81 / 9)
+# ==========================
+# Excercise 3: 
+name = "Tran Nguyen Ngoc"
+birth_year = 2007
+current_year = 2026
+city = "HCM city"
+target_job = "Software Engineer"
+print(name)
+print(birth_year)
+print(current_year)
+print(city)
+print(target_job)
+# ==========================
+# Excercise 4:
+age = current_year - birth_year
+print(age)
+# ==========================
+# Excercise 5: 
+ausbildung_year = 2027
+years_left = ausbildung_year - current_year 
+print(years_left)
+# ==========================
+# Excercise 6:
+age = age + 1
+print(age)
+# ==========================
+# Excercise 7: 
+favorite_food = "Pho"
+favorite_programming_language = "Python" 
+phone_number = "0123456789"
+print(favorite_food)
+print(favorite_programming_language)
+print(phone_number)
+
+
+
+
+
+
+
+
