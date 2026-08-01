30f7503b27f8365d7e7b3d48c01bd7c0648ea844
Tran Ngoc <tranngoc08012007@gmail.com>
2026-08-01 21:36:07 +0700

    style: format code with black

--- Phase_01 — Python Fundamentals/Lesson_01_Environment_Setup/exercises.py
+++ Phase_01 — Python Fundamentals/Lesson_01_Environment_Setup/exercises.py
@@ -48,15 +48,15 @@
 # My solutions
 # ==========================
 # Excercise 1:
-print("Hello, world!") 
+print("Hello, world!")
 # ==========================
 # Excercise 2:
-print(15 +25)
-print(100 -36)
+print(15 + 25)
+print(100 - 36)
 print(8 * 7)
 print(81 / 9)
 # ==========================
-# Excercise 3: 
+# Excercise 3:
 name = "Tran Nguyen Ngoc"
 birth_year = 2007
 current_year = 2026
@@ -72,27 +72,19 @@ print(target_job)
 age = current_year - birth_year
 print(age)
 # ==========================
-# Excercise 5: 
+# Excercise 5:
 ausbildung_year = 2027
-years_left = ausbildung_year - current_year 
+years_left = ausbildung_year - current_year
 print(years_left)
 # ==========================
 # Excercise 6:
 age = age + 1
 print(age)
 # ==========================
-# Excercise 7: 
+# Excercise 7:
 favorite_food = "Pho"
-favorite_programming_language = "Python" 
+favorite_programming_language = "Python"
 phone_number = "0123456789"
 print(favorite_food)
 print(favorite_programming_language)
 print(phone_number)
-
-
-
-
-
-
-
-
